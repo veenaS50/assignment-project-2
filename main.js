72c40b6fa6ed2ba365d9364291b336c0e1f836c4
veenaS50 <subbuveena18@gmail.com>
2021-07-30 13:45:26 +0530

    2nd commit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const textBox = document.getElementById('input-text')
 const button = document.getElementById('search')
 
 button.addEventListener('click', () => {
+    const container = document.getElementById('card-container')
+    container.innerHTML = ""
     const search = textBox.value 
     const xhr = new XMLHttpRequest ()
     const url = `https://www.googleapis.com/books/v1/volumes?q=${search}`
@@ -20,26 +22,29 @@ button.addEventListener('click', () => {
                 const publisher = document.createElement('p')
                 var a = document.createElement('a')
 
-                if(userData.items[i].volumeInfo.author === "undefined"){
-                    author.innerHTML("")
-                }
 
                 image.setAttribute('src', userData.items[i].volumeInfo.imageLinks.smallThumbnail)
                 title.innerHTML = userData.items[i].volumeInfo.title;
-                author.innerHTML = 'Author: ' + userData.items[i].volumeInfo.authors;
-                publisher.innerHTML = 'Publisher: ' + userData.items[i].volumeInfo.publisher;
+
+                if(userData.items[i].volumeInfo.authors === undefined){
+                    author.innerHTML = "Author: Info not available"
+                } else {
+                    author.innerHTML = 'Author: ' + userData.items[i].volumeInfo.authors;
+                }
+                
+                if(userData.items[i].volumeInfo.publisher === undefined){
+                    publisher.innerHTML = "Publisher: Info not available"
+                } else {
+                    publisher.innerHTML = 'Publisher: ' + userData.items[i].volumeInfo.publisher;
+                }
+
+                a.href = userData.items[i].volumeInfo.previewLink;
                 a.textContent = "Read More"
                 a.target = "_blank"
-                a.href = userData.items[i].volumeInfo.previewLink;
                 a.className = "read-more-button"
 
-                const container = document.getElementById('card-container')
-                // container.appendChild(image)
-                // container.appendChild(title)
-                // container.appendChild(author)
-                // container.appendChild(publisher)
-                // container.appendChild(a)
-
+                // const container = document.getElementById('card-container')
+            
                 const book_item = document.createElement('div')
                 book_item.setAttribute("class", "book-item-list")
 
@@ -58,29 +63,6 @@ button.addEventListener('click', () => {
                 container_right.appendChild(publisher)
                 container_right.appendChild(a)
             }
-
-            // const image = document.createElement('img')
-            // const title = document.createElement('h1')
-            // const author = document.createElement('h2')
-            // const publisher = document.createElement('h3')
-            // var a = document.createElement('a')
-
-            // title.innerHTML = userData.items[0].volumeInfo.title;
-            // author.innerHTML = userData.items[0].volumeInfo.authors;
-            // image.setAttribute('src', userData.items[0].volumeInfo.imageLinks.smallThumbnail)
-            // publisher.innerHTML = userData.items[0].volumeInfo.publisher;
-            // a.textContent = "Read More"
-            // a.target = "_blank"
-            // a.href = userData.items[0].volumeInfo.previewLink;
-
-            // image.className="trial-image"
-
-            // const container = document.getElementById('card-container')
-            // container.appendChild(image)
-            // container.appendChild(title)
-            // container.appendChild(author)
-            // container.appendChild(publisher)
-            // container.appendChild(a)
         }
     }
     xhr.send()
